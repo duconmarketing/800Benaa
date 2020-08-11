6d71fcc260436a7af2ed85a2eba1c954b9ae059d
sabin ducon <sabinchacko03>
2020-08-11 09:25:35 +0400

    updated by sabin

--- application/mail/new_order_notification.php
+++ application/mail/new_order_notification.php
@@ -17,7 +17,7 @@ ob_start();
 <html>
     <head>
     </head>
-    <body style="width: 70%;alignment: center;">
+    <body style="width: 75%;alignment: center;">
         <?php $header = trim(\Config::get('community_store.receiptHeader')); ?>
 
         <?php
